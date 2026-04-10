cfe24eb981208547c9f7f7a3aa38193ed2a4e124
yashmahindroo18 <yashmahindroo10@gmail.com>
2026-04-10 17:56:08 +0530

    Implemented methods to add employees and tasks

--- ResourceManager.java
+++ ResourceManager.java
@@ -4,3 +4,10 @@ class ResourceManager {
     private HashMap<Integer, Employee> employees = new HashMap<>();
     private HashMap<Integer, Task> tasks = new HashMap<>();
 }
+public void addEmployee(Employee e) {
+    employees.put(e.getId(), e);
+}
+
+public void addTask(Task t) {
+    tasks.put(t.getId(), t);
+}
